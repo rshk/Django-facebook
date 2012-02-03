89e86d93c4684eb9a777e4160ffddc5be24b55cb
Samuele Santi <redshadow@hackzine.org>
2012-02-04 00:34:46 +0100

    Using full absolute URL instead of protocol-relative one.

--- django_facebook/context_processors.py
+++ django_facebook/context_processors.py
@@ -7,4 +7,5 @@ def facebook(request):
     protocol_relative_url = request.build_absolute_uri('/')
     
     context['protocol_relative_url'] = "//%s" % urlparse.urlparse(protocol_relative_url).netloc
+    context['absolute_root_url'] = protocol_relative_url
     return context
